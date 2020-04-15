5a59a5e0309d98038e815b2bb61530b6ed5ddc66
David Goodwin <david@alchemycodelab.com>
2020-04-14 22:05:48 -0700

    It actually works now.

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # movie file renamer
 
-work in progress.  
+Renames scene-style movie files to "Movie Name - year.ext".
 
+It announces the title and release data, renames the file, and sets the atime and ctime of the file to the release date.
+
+example:
+
+``` sh
+$ node index.js Citizen.Kane.1941.DVDRip.x264.AC3.mkv
+Citizen Kane was released on 05 Sep 1941
+$ ls
+-rw-------   1 user user    0 Sep  5  1941 Citizen Kane (1941).mkv
+```
+## Warranty
 If you break it, you get to keep both pieces.
+
+
+Get your API key from [omdbapi.com](http://www.omdbapi.com/apikey.aspx) and put it in .env
+
+Also, donate to them, cuz they rock.
+
+I don't know if OMDB has a rate-limited API.  I haven't run this on a whole directory yet, just onesies, so I don't know.  YMMV.  Don't abuse APIs.  And donate.
+
+## Plans
+* convert this into a CLI app that can do all tasks optionally, break out the API call, ctime update, rename, etc. to flags or default behaviors.  Someday, maybe the next time filebot utterly fails me.
+* add support for other APIs
--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 const chalk = require('chalk');
 const axios = require('axios');
 const processName = require('./lib/processName');
+
 
 const movieFileName = process.argv[2];
 if(!movieFileName) {
   console.log(chalk.red('No movie name supplied!'));
   process.exit(1)
 }
-const {search, year} = processName(movieFileName);
+const {title, year} = processName(movieFileName);
+const ext = path.extname(movieFileName);
 
-axios.get(`https://www.omdbapi.com/?apikey=${process.env.APIKEY}&t=${search}&y=${year}`)
+axios.get(`https://www.omdbapi.com/?apikey=${process.env.APIKEY}&t=${title}&y=${year}`)
   .then(res => {
     const movie = res.data;
+    const releaseDate = new Date(movie.Released);
+    fs.utimesSync(movieFileName, releaseDate, releaseDate);
     if(!movie.Title) throw new Error('No movie title was found.')
     console.log(`${movie.Title} was released on ${movie.Released}`);
+    const newName = `${movie.Title} (${year})${ext}`
+    fs.renameSync(movieFileName, newName);
   })
   .catch(err => console.error(chalk.red(` * ${err}`)));
